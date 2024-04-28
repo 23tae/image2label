b4a1cae8f72a7b4a76e9aff28a531f7a0e36a352
23tae <taehoonkim.dev@gmail.com>
2024-04-29 02:49:40 +0900

    Feat: 환경변수, 파일명 저장

---
+++ variables.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+import boto3
+
+
+def get_variables():
+    bucket, folder = get_env()
+    filenames = get_filenames(bucket, folder)
+    return bucket, folder, filenames
+
+
+def get_env() -> tuple:
+    load_dotenv()
+    bucket = os.getenv('BUCKET_NAME')
+    folder = os.getenv('FOLDER_NAME')
+    return bucket, folder
+
+
+def get_filenames(bucket, folder) -> list:
+    s3 = boto3.resource("s3")
+    s3_bucket = s3.Bucket(bucket)
+    files = [f.key.split(folder + "/")[1]
+             for f in s3_bucket.objects.filter(Prefix=folder).all()]
+    return files
